ccf88fcc87f408efcf3fa426cc7b5f8f586ce84f
griffith <xjulianbarragan@gmail.com>
2026-09-01 22:31:23 +0200

    added test for arithmetic expressions

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -2,51 +2,60 @@ module APL.Eval
   (
   Val(..),
   eval,
+  divByZeroErr,
+  negExpErr,
   )
 where
 
 import APL.AST (Exp(..))
 
+
+-- Value
 data Val = ValInt Integer
   deriving (Eq, Show)
 
-
+-- Error Messages
 type Error = String
+divByZeroErr :: Error
+divByZeroErr = "Division by zero error"
+negExpErr :: Error
+negExpErr = "Negative exponent error"
 
 
+-- Eval function to evaluate expressions into values
 eval :: Exp -> Either Error Val
-
+-- Constructor
 eval (CnstInt x) = Right $ ValInt x
-
+-- Addition
 eval (Add e1 e2) = 
   case(eval e1, eval e2) of
     (Left err, _) -> Left err
     (_, Left err) -> Left err
     (Right (ValInt x), Right (ValInt y)) -> Right $ ValInt $ x+y
-
+-- Subtraction
 eval (Sub e1 e2) = 
   case(eval e1, eval e2) of 
     (Left err, _) -> Left err
     (_, Left err) -> Left err
     (Right (ValInt x), Right (ValInt y)) -> Right $ ValInt $ x-y
-
+-- Multiplication
 eval (Mul e1 e2) =
   case(eval e1, eval e2) of
     (Left err, _) -> Left err
     (_, Left err) -> Left err
     (Right (ValInt x), Right (ValInt y)) -> Right $ ValInt $ x*y
-
+-- Division (integer)
 eval (Div e1 e2) = 
   case(eval e1, eval e2) of 
-    (_, Right(ValInt 0)) -> Left $ "Division by zero error"
+    (_, Right(ValInt 0)) -> Left $ divByZeroErr
     (Left err, _) -> Left err
     (_, Left err) -> Left err
     (Right(ValInt x), Right(ValInt y)) -> Right $ ValInt $ x `div` y
-
+-- Power (integer)
 eval (Pow e1 e2) =
   case(eval e1, eval e2) of
     (Left err, _) -> Left err
     (_, Left err) -> Left err
     (Right(ValInt x), Right(ValInt y))
-      | y < 0 -> Left $ "Negative exponent"
+      | y < 0 -> Left $ negExpErr
       | otherwise -> Right $ ValInt $ x^y
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,16 +1,50 @@
 module APL.Eval_Tests (tests) where
 
 import APL.AST (Exp(..))
-import APL.Eval (Val(..), eval)
+import APL.Eval (Val(..), eval, divByZeroErr, negExpErr)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 
-evalTest :: TestTree
-evalTest = testCase "Testing eval func" $ eval (CnstInt 1) @?= ValInt 1
+-- Test for Eq eval(Exp) = Val of same integer
+evalEqTest :: TestTree
+evalEqTest = testCase "Testing eval returns correct value" $ eval (CnstInt 1) @?= Right (ValInt 1)
+-- Test Add
+evalAddTest :: TestTree
+evalAddTest = testCase "Testing eval correctly adds 2+2=4" $ eval (Add (CnstInt 2) (CnstInt 2)) @?= Right (ValInt 4)
+evalNegAddTest :: TestTree -- negative Add
+evalNegAddTest = testCase "Testing eval correctly add (-2)+2=0" $ eval (Add (CnstInt (-2)) (CnstInt 2)) @?= Right (ValInt 0)
+-- Test Sub
+evalSubTest :: TestTree
+evalSubTest = testCase "Testing eval correctly subtracts 2-2=0" $ eval (Sub (CnstInt 2) (CnstInt 2)) @?= Right (ValInt 0)
+evalNegSubTest :: TestTree -- negative Val from sub
+evalNegSubTest = testCase "Testing eval correctly subtracts 2-4=-2" $ eval (Sub (CnstInt 2) (CnstInt 4)) @?= Right (ValInt (-2))
+-- Test Mul
+evalMulTest :: TestTree
+evalMulTest = testCase "Testing eval correctly multiplies 4*4=16" $ eval (Mul (CnstInt 4) (CnstInt 4)) @?= Right (ValInt 16)
+-- Test Div
+evalDivTest :: TestTree
+evalDivTest = testCase "Testing eval correctly divides 12/3=4" $ eval (Div (CnstInt 12) (CnstInt 3)) @?= Right (ValInt 4)
+evalDivByZeroTest :: TestTree
+evalDivByZeroTest = testCase "Testing eval raises div-by-zero error" $ eval (Div (CnstInt 1) (CnstInt 0)) @?= Left divByZeroErr
+-- Test Pow
+evalPowTest :: TestTree
+evalPowTest = testCase "Testing eval correctly applies power 2^3=8" $ eval (Pow (CnstInt 2) (CnstInt 3)) @?= Right (ValInt 8)
+evalNegExpPowTest :: TestTree
+evalNegExpPowTest = testCase "Testing eval raises neg-exp error" $ eval (Pow (CnstInt 1) (CnstInt (-1))) @?= Left negExpErr
+
 tests :: TestTree
 tests =
   testGroup
-    "Evaluation"
+    "Evaluation tests"
     [
-      evalTest
+      evalEqTest,
+      evalAddTest,
+      evalSubTest,
+      evalNegSubTest,
+      evalNegAddTest,
+      evalMulTest,
+      evalDivTest,
+      evalDivByZeroTest,
+      evalPowTest,
+      evalNegExpPowTest
     ]

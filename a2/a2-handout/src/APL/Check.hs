81b14e5751a6a4fba9916526223f8ccacb40078c
OskarLinke <olinke2@gmail.com>
2026-09-18 13:40:55 +0200

    setup for Check

--- a2/a2-handout/src/APL/Check.hs
+++ a2/a2-handout/src/APL/Check.hs
@@ -2,9 +2,21 @@ module APL.Check (checkExp, Error) where
 
 import APL.AST (Exp (..), VName)
 
+import Control.Monad (ap, liftM)
+
 type Error = String
 
 newtype CheckM a = CheckM () -- TODO - give this a proper definition.
+
+instance Functor CheckM where
+  fmap = liftM
+
+instance Applicative CheckM where
+  pure x = undefined
+  (<*>) = ap
+
+instance Monad CheckM where
+  CheckM x >>= f = undefined 
 
 check :: Exp -> CheckM ()
 check = undefined

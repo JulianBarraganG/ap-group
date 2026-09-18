b4a5bc1d987b6f5ce0ee2591e29af6567c104079
OskarLinke <olinke2@gmail.com>
2026-09-18 22:15:24 +0200

    finished code and I believe also the tests

--- a2/a2-handout/src/APL/Check.hs
+++ a2/a2-handout/src/APL/Check.hs
@@ -29,6 +29,20 @@ askVars = CheckM $ \vars -> Right vars
 noVars :: VName -> CheckM a
 noVars var = CheckM $ \_vars -> Left ("Variable not in scope: " ++ var)
 
+
+varsExtend :: VName -> EnvVars -> EnvVars
+varsExtend v env = v : env
+
+localVars :: (EnvVars -> EnvVars) -> CheckM a -> CheckM a
+localVars f (CheckM m) = CheckM $ \envVars -> m (f envVars)
+
+
+simpleCheckHelper :: Exp -> Exp -> CheckM ()
+simpleCheckHelper e1 e2 = do
+  _ <- check e1
+  _ <- check e2
+  pure ()
+
 check :: Exp -> CheckM ()
 check (CstInt _) = pure ()
 
@@ -38,12 +52,60 @@ check (Var v) = do
   vars <- askVars
   if (elem v vars) then pure() else (noVars v)
 
-check (Add e1 e2) = do 
+check (Add e1 e2) = 
+  simpleCheckHelper e1 e2
+
+check (Mul e1 e2) =  
+  simpleCheckHelper e1 e2
+
+check (Sub e1 e2) = 
+  simpleCheckHelper e1 e2
+
+check (Div e1 e2) = 
+  simpleCheckHelper e1 e2
+
+check (Pow e1 e2) = 
+  simpleCheckHelper e1 e2
+
+check (Eql e1 e2) =
+ simpleCheckHelper e1 e2
+
+check (If e1 e2 e3) = do 
   _ <- check e1
   _ <- check e2
+  _ <- check e3
   pure ()
 
-check _ = undefined
+check (Let v e1 e2) = do
+  _ <- check e1 -- Evaluated in outer env
+  _ <- localVars (varsExtend v) $ check e2 
+  pure ()
+
+check (ForLoop (v1, e1) (v2, e2) e3) = do
+  simpleCheckHelper e1 e2 -- Evaluated in the outer env
+  _ <- localVars (varsExtend v1 . varsExtend v2) $ check e3
+  pure ()
+
+check (Lambda v e) = do
+  _ <- localVars (varsExtend v) $ check e
+  pure ()
+
+check (Apply e1 e2) = 
+  simpleCheckHelper e1 e2
+
+check (TryCatch e1 e2) = 
+  simpleCheckHelper e1 e2
+
+check (Print _ e) = do
+  _ <- check e
+  pure ()
+
+check (KvPut e1 e2) = 
+  simpleCheckHelper e1 e2
+
+check (KvGet e) = do
+  _ <- check e
+  pure ()
 
 checkExp :: Exp -> Maybe Error
 checkExp e1 = 
--- a2/a2-handout/src/APL/Check_Tests.hs
+++ a2/a2-handout/src/APL/Check_Tests.hs
@@ -27,6 +27,44 @@ tests =
       testPos (CstInt 2),
       testNeg(Var ("x")),
       testPos (Add (CstInt 2) (CstInt 2)),
-      testNeg (Add (CstInt 2) (Var "x"))
+      testNeg (Add (CstInt 2) (Var "x")),
 
+      testNeg (Add (Let "x" (CstInt 1) (Var "x")) (Var "x")), --No leakage
+
+      testNeg (If (CstBool True) (CstInt 3) (Var "x")),
+
+      testPos (If (CstInt 3) (CstBool False) (CstBool True)),
+
+      testPos (Let "x" (CstInt 3) (Add (Var "x") (CstInt 5))),
+      testNeg (Let "x" (Add (Var "x") (CstInt 5)) (CstInt 3)),
+      testPos (Let "x" (CstInt 1) (Lambda "y" (Add (Var "x") (Var "y")))),
+
+
+      testPos (ForLoop ("p", (CstInt 0)) ("i", (CstInt 10)) (Add (Var "p") (Var "i"))),
+      testNeg (ForLoop ("p", (Add (Var "i") (CstInt 2))) ("i", (CstInt 10)) (Add (Var "p") (Var "i"))),
+      testNeg (ForLoop ("p", (Add (Var "p") (CstInt 2))) ("i", (CstInt 10)) (Add (Var "p") (Var "i"))),
+
+      testPos (Lambda "x" (CstBool True)),
+      testPos (Lambda "x" (Add (Var "x") (CstInt 2))),
+      testNeg (Lambda "y" (Add (Var "x") (CstInt 2))),
+
+      testPos (Apply (CstInt 3) (CstInt 3)), --Wouldn't evaluate since CstInt doesn't evaluate to a ValFun, 
+      --but typechecker obviously only cares about whether variables are in scope
+      --
+      testPos (Apply (Lambda "x" (Add (Var "x") (CstInt 2))) (CstInt 3)),
+      testPos (Apply (Lambda "x" (Add (Var "x") (CstInt 2))) (CstBool True)), -- also wouldn't eval, check is fine
+      testNeg (Apply (Lambda "x" (Add (Var "y") (CstInt 2))) (CstInt 3)),
+      testNeg (Apply (Lambda "x" (Add (Var "x") (CstInt 2))) (Var "y")),
+
+      testPos (TryCatch (CstInt 2) (CstBool True)),
+      testNeg (TryCatch (CstInt 2) (Var "x")), -- fails, even though e2 wouldn't be evaluated
+
+      testNeg (Print "x" (Var "x")), --string isn't a var
+
+      testNeg (KvPut(Apply (Lambda "x" (Add (Var "x") (CstInt 2))) (CstBool True)) (Var "x")), --e1 e2 have different envs. 
+
+      testPos (KvPut (CstInt 1) (CstBool True)), 
+
+      testPos (KvGet (CstInt 1)), 
+      testNeg (KvGet (Var "x"))
     ]
